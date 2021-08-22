9a53b78e3ce6d682b664cdd2af723647f22eada5
ian lombu <citrakemurnian@gmail.com>
2021-08-22 14:10:28 +0700

    API Midtranss Callback

---
+++ app/Http/Controllers/API/MidtransController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Midtrans\Config;
+use Midtrans\Notification;
+
+class MidtransController extends Controller
+{
+    public function callback(Request $request)
+    {
+        // Set Konfigurasi Midtrans
+        Config::$serverKey = config('service.midtrans.serverKey');
+        Config::$isProduction = config('service.midtrans.isProduction');
+        Config::$isSanitized = config('service.midtrans.isSanitized');
+        Config::$is3ds = config('service.midtrans.is3ds');
+
+        // Buat instance midtrans notification
+        $notification = new Notification();
+
+        // Assign ke variable untuk memudahkan coding
+        $status = $notification->transaction_status;
+        $type = $notification->payment_type;
+        $fraud = $notification->fraud_status;
+        $order_id = $notification->order_id;
+
+        // Cari transaksi berdasarkan ID
+        $transaction = Transaction::findOrFail($order_id);
+
+        // Handle notifikasi status midtrans
+        if ($status == 'capture') {
+            if ($type == 'credit_card') {
+                if ($fraud == 'challenge') {
+                    $transaction->status = 'PENDING';
+                } else {
+                    $transaction->status = 'SUCCESS';
+                }
+            }
+        } else if ($status == 'settlement') {
+            $transaction->status = 'SUCCESS';
+        } else if ($status == 'pending') {
+            $transaction->status = 'PENDING';
+        } else if ($status == 'deny') {
+            $transaction->status = 'CANCELLED';
+        } else if ($status == 'expire') {
+            $transaction->status = 'CANCELLED';
+        } else if ($status == 'cancel') {
+            $transaction->status = 'CANCELLED';
+        }
+
+        // Simpan transaksi
+        $transaction->save();
+    }
+}
